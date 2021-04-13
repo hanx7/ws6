3f2aee4813952c9b365ace2a210adee2b6c69d84
Xiao Han <hanx7@student.unimelb.edu.au>
2021-04-13 13:16:39 +1000

    second..

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,12 +1,22 @@
 const express = require('express')
 
-// add our router
+// add our router 
 const authorRouter = express.Router()
 
 // require the author controller
 const authorController = require('../controllers/authorController.js')
 
-// handle the GET request toget all authors
-authorRouter.get('/',authorController.getAllAuthors)
+// handle the GET request to get all authors
+authorRouter.get('/', (req, res) => authorController.getAllAuthors(req, res))
 
+// handle the GET request to get one author
+authorRouter.get('/:authorId', (req, res) => authorController.getOneAuthor(req, res))
+
+// handle POST requests to add one author
+authorRouter.post('/', (req, res) => authorController.addAuthor(req, res))
+
+// handle POST requests to update an author
+authorRouter.post('/update', (req, res) => authorController.updateAuthor(req, res))
+
+// export the router
 module.exports = authorRouter
